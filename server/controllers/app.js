0fe2d4ab81b1401f34abdf53fc42c72fe7d6a25a
minigoat <johnchenxx@126.com>
2019-06-18 23:50:45 +0800

    解析dapen

--- server/controllers/app.js
+++ server/controllers/app.js
@@ -1,9 +1,6 @@
 const reptile_dapenti = require('../service/dapenti');
 let dapenti = async (ctx, next) => {
-    console.log('test111')
     let result = await reptile_dapenti('http://www.dapenti.com/blog/more.asp?name=xilei&id=141266');
-    console.log('result', result);
-    console.error('test222');
     ctx.response.body = await '<h1>Hello World!</h1>';
 };
 
--- server/service/dapenti.js
+++ server/service/dapenti.js
@@ -8,14 +8,11 @@ const reptile_dapenti = async function(url) {
     console.log('reptile_dapenti-1');
     try {
         let res = await get(url);
-        let text = await analyze_dapenti(res);
-        // let n=0;
-        // for (let item of text) {
-        //     console.log("n:" + n);
-        //     console.log(item);
-        //     n++;
-        // }
-        return text[0];
+        let newsList = await analyze_dapenti(res);
+        // console.log('newsList', newsList);
+        console.log('JSON.stringify(newsList)', JSON.stringify(newsList));
+        newsList = suppl_unFindIndex(newsList);
+        return newsList;
     } catch(error) {
         return error;
     }    
@@ -34,30 +31,65 @@ const get = (url) => {
 const titleRegExp = new RegExp('^【\\d+】');
 const analyze_dapenti = function(res) {
     let newsList = [];
-    let pList = [];
     let news = {};
     let $ = cheerio.load(res.text);
+    let isEnd = false;
     $('div.oblog_text p').each((idx, ele) => {
         let text = _.trim($(ele).text());
         if (text!= null && text != '') {
-            if (titleRegExp.test(text)) {
-                news['title'] = text;
-                news['content'] = [];
+            if (_.startsWith(text, '来源：喷嚏网')) {
+                isEnd = true;
             }
-
-            pList.push($(ele).text());
-            console.log(`idx:${idx}`);
-            console.log(text);
-            
+            if (!isEnd) {
+                if (titleRegExp.test(text)) {
+                    let index = text.substr(text.indexOf("【") + 1, text.indexOf("】") - text.indexOf("【") - 1);
+                    if (news['index'] != null) {
+                        newsList.push(news);
+                        news = {};
+                    }
+                    news['title'] = text;
+                    news['index'] = index;
+                    news['body'] = [];
+                } else {
+                    if (news['index'] != null) {
+                        news['body'].push({
+                            type: 'text',
+                            content: text
+                        });
+                    }
+                }
+            }          
         }
-        if ($(ele).find('img').length > 0) {            
-            console.log(`idx:${idx}`);
-            $(ele).find('img').each((imgIdx, imgEle) => {
-                console.log(`图片src=${$(imgEle).attr('src')}`);
-            });            
+        if ($(ele).find('img').length > 0) {
+            if (news['index'] != null) {
+                $(ele).find('img').each((imgIdx, imgEle) => {
+                    news['body'].push({
+                        type: 'img',
+                        content: $(imgEle).attr('src')
+                    });
+                });
+            }                      
         }
     });
-    return Promise.resolve(pList);
+    newsList.push(news);
+    return Promise.resolve(newsList);
+}
+
+const suppl_unFindIndex = function(newsList) {
+    let unFindIndexList = [];
+    let n=1;
+    let indexList = [];
+    let maxIndex = newsList[newsList.length-1].index;
+    _.forEach(newsList, (news, index) => {
+        indexList.push(news.index);
+    });
+    for (let index=maxIndex;index > 0;index--) {
+        if (_.indexOf(indexList, `${index}`) == -1) {
+            unFindIndexList.push(index);
+        }
+    }
+    console.log(`unFindIndexList:${unFindIndexList}`);
+    return newsList;
 }
 
 
